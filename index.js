225b184042d1d7b9c602e5a377f158ef85f7ff89
christopherwitcher <christopher.witcher@live.com>
2018-07-24 12:31:43 -0700

    Strong Tag Added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     //response.end("Hello World!");
-    response.end("Hello, Mr. Witcher :D");
+    response.end(<strong>"Hello, Mr. Witcher :D"</strong>);
 
 
 });
